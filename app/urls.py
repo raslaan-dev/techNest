6383c39e0ffcbd58c73dc81bd9ec168aa723a2ba
raslaan-dev <raslaan2004@gmail.com>
2024-12-22 16:30:08 +0500

    working cursor

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    
-    
+    path('', views.index, name='home'),  
+    path('index/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('shop/', views.shop, name='shop'),
+    path('sale/', views.sale, name='sale'),
+    path('help/', views.help, name='help'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
+
+def about(request):
+    return render(request, 'app/about.html')
+
+def shop(request):
+    return render(request, 'app/shop.html')
+
+def sale(request):
+    return render(request, 'app/sale.html')
+
+def help(request):
+    return render(request, 'app/help.html')
